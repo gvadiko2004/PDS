9a09b8dbbbe262b2175a37398be3594d1b86ccc8
gvadiko2004 <archilbigvava@gmail.com>
2024-11-21 05:15:35 +0200

    commit

--- js/swiper-min.js
+++ js/swiper-min.js
@@ -3,8 +3,25 @@ const swiper = new Swiper(".swiper-feedbacks", {
   loop: true,
   effect: "fade",
   fadeEffect: {
-    crossFade: true, 
+    crossFade: true,
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".btns-swiper-prev",
+    prevEl: ".btns-swiper-next",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const swiperPartners = new Swiper(".swiper-partners", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 2,
+
   pagination: {
     el: ".swiper-pagination",
   },
